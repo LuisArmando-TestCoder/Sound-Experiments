a0a725826b86b7085970731463bafca1f62042ab
LuisArmando-TestCoder <luisarmando.murillobaltodano@gmail.com>
2019-03-26 20:13:20 -0600

    update: floating smooth balls interacting

--- experiments/songHeroBanner/js/songHeroBanner.js
+++ experiments/songHeroBanner/js/songHeroBanner.js
@@ -4,14 +4,14 @@
   }
   function drawShapes(array, func) {
     function circles() {
-      array.forEach((obj) => {
+      array.forEach(obj => {
         ctx.beginPath();
         ctx.arc(obj.x, obj.y, obj.r, 0, Math.PI * 2);
         if (typeof func) func(obj);
       });
     }
     function rects() {
-      array.forEach((obj) => {
+      array.forEach(obj => {
         if (typeof func) func(obj);
         ctx.fillRect(obj.x, obj.y, obj.width, obj.height);
       });
@@ -27,7 +27,7 @@
     });
   }
   function appendCanvas(parent) {
-    const cnv = document.createElement('canvas');
+    const cnv = document.createElement("canvas");
     quicker().canvasManageSize(cnv);
     parent.appendChild(cnv);
     return cnv;
@@ -37,40 +37,51 @@
   }
   function animate() {
     clearFrame(ctx);
-    
-    drawShapes(circles, (obj) => {
+
+    drawShapes(circles, obj => {
       ctx.fillStyle = obj.color;
       ctx.fill();
     }).circles();
-    
-    drawShapes(audioBars, (obj) => {
+
+    drawShapes(audioBars, obj => {
       ctx.fillStyle = obj.color;
       ctx.fill();
     }).rects();
     let barIndex = 0;
-    const _size = 7, _spacing = 2;
-    updateShapes(audioBars, (obj) => {
-      obj.x = (c.width / 2) + barIndex * (_size + _spacing) - barsAmount * _size / 2 - _spacing * barsAmount / 2;
+    const _size = 7,
+      _spacing = 2;
+    updateShapes(audioBars, obj => {
+      obj.x =
+        c.width / 2 +
+        barIndex * (_size + _spacing) -
+        (barsAmount * _size) / 2 -
+        (_spacing * barsAmount) / 2;
       obj.y = c.height / 2;
-      if(songContext) {
+      if (songContext) {
         obj.height = -songContext.getFrequency().array[barIndex];
         barIndex++;
       }
     });
-    
-    updateShapes(circles, (obj) => {
-      obj.x+=0.05 * obj.r;
-      if(obj.x - obj.r > c.width) {
+
+    updateShapes(circles, obj => {
+      obj.x += 0.05 * obj.r;
+      let frequency = 0;
+      if (songContext) {
+        let audioArray = songContext.getFrequency().array;
+        frequency = audioArray.reduce((a, b) => a + b) * audioArray.length;
+      }
+      if (obj.x - obj.r > c.width) {
         obj.x = -obj.r;
         obj.y = quicker().makeIntegerRandom(0, c.height);
       }
+      obj.r = obj.size + frequency * 0.000001;
       obj.color = `rgba(255, 255, 255, 
       ${obj.opacity.number(10) * 0.025})`;
     });
-    
+
     ctx.beginPath();
     ctx.stroke();
-    ctx.strokeStyle = '#ffffbf';
+    ctx.strokeStyle = "#ffffbf";
     requestAnimationFrame(animate);
   }
   function repeat(times, func) {
@@ -81,67 +92,73 @@
   function moveAround(n) {
     let direction = n / quicker().makeFloatingRandom(50, 100);
     function number(max) {
-      if (n > max || n < 0) direction = -direction; 
+      if (n > max || n < 0) direction = -direction;
       n += direction;
       return n;
     }
     return {
-      number: number,
-    }
+      number: number
+    };
   }
-  const c = appendCanvas(document.getElementById('starsCnvParent'));
-  const ctx = c.getContext('2d');
+  const c = appendCanvas(document.getElementById("starsCnvParent"));
+  const ctx = c.getContext("2d");
   const circles = [];
   const audioBars = [];
-  const nodesAmount = 89, barsAmount = 21;
-  const startSongBtn = document.getElementById('startSong');
-  
+  const nodesAmount = 89,
+    barsAmount = 21;
+  const startSongBtn = document.getElementById("startSong");
+
   let audioFlag = false;
   let songContext = 0;
   let audio = new Audio();
-  audio.src = `calm0${quicker().makeIntegerRandom(0, 1)}.mp3`;
-  // audio.src = 'calm02.mp3';
-  
-  repeat(nodesAmount, (i) => {
+  audio.src = "calm00.mp3";
+
+  repeat(nodesAmount, i => {
     let size = quicker().makeFloatingRandom(0.1, 30); // 30 for calmness
     let opacity = moveAround(quicker().makeIntegerRandom(1, 9));
     create(circles, {
       x: quicker().makeIntegerRandom(0, c.width - size * 2),
       y: quicker().makeIntegerRandom(0, c.height - size * 2),
       r: size,
+      size: size,
       opacity: opacity,
       color: `rgba(255, 255, 255, ${opacity.number(10) * 0.1})`,
-      velocity: quicker().makeIntegerRandom(3, 6) * size,
+      velocity: quicker().makeIntegerRandom(3, 6) * size
     });
   });
-  
-  repeat(barsAmount, (i) => {
-    const _size = 7, _spacing = 2;
+
+  repeat(barsAmount, i => {
+    const _size = 7,
+      _spacing = 2;
     create(audioBars, {
-      x: (c.width / 2) + i * (_size + _spacing) - barsAmount * _size / 2 - _spacing * barsAmount / 2,
+      x:
+        c.width / 2 +
+        i * (_size + _spacing) -
+        (barsAmount * _size) / 2 -
+        (_spacing * barsAmount) / 2,
       y: c.height / 2,
       width: _size,
       height: songContext,
-      color: 'rgb(255, 255, 255)',
+      color: "rgb(255, 255, 255)"
     });
   });
-  
+
   // startSongBtn.style.setProperty('transform', 'scale(0)');
-    // startSongBtn.style.setProperty('transform', 'scale(1)');
-  startSongBtn.addEventListener('click', () => {
-    if(!songContext) songContext = quicker().analyseAudio(audio);
-    if(!audioFlag) {
+  // startSongBtn.style.setProperty('transform', 'scale(1)');
+  startSongBtn.addEventListener("click", () => {
+    if (!songContext) songContext = quicker().analyseAudio(audio);
+    if (!audioFlag) {
       audio.play();
-      startSongBtn.style.setProperty('transform', 'scale(0.25)');
-      startSongBtn.style.setProperty('border-radius', '0');
+      startSongBtn.style.setProperty("transform", "scale(0.25)");
+      startSongBtn.style.setProperty("border-radius", "0");
     } else {
       audio.pause();
-      startSongBtn.style.setProperty('transform', 'scale(1)');
-      startSongBtn.style.setProperty('border-radius', '50%');
+      startSongBtn.style.setProperty("transform", "scale(1)");
+      startSongBtn.style.setProperty("border-radius", "50%");
     }
     audioFlag = !audioFlag;
   });
   animate();
   // quicker().showFrameRate();
-}());
+})();
 // https://luisarmando-testcoder.github.io/keeper/mp3/calm00.mp3
